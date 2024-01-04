77f6aa1e7e284b8b077889d7b6b2998f50e65545
Fabricio <fabriciossuhet@gmail.com>
2024-01-04 17:57:28 -0300

    adicionando teste da interface

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { HousingLocationComponent } from '../housing-location/housing-location.component';
+import { HousingLocation } from '../housing-location';
 
 
 @Component({
@@ -14,5 +15,19 @@ import { HousingLocationComponent } from '../housing-location/housing-location.c
   styleUrl: './home.component.css'
 })
 export class HomeComponent {
+
+  readonly baseUrl = 'https://angular.io/assets/images/tutorials/faa';
+
+  housingLocation: HousingLocation = {
+    id: 9999,
+    name: 'Test home',
+    city: 'Test city',
+    state: 'ST',
+    photo: `${this.baseUrl}/example-house.jpg`,
+    availableUnits: 99,
+    wifi: true,
+    laundry: false,
+
+  }
 
 }
