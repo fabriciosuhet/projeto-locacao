a715a021c722f0ed5a0d950cd0c2e5984ff46828
Fabricio <fabriciossuhet@gmail.com>
2024-01-04 17:50:07 -0300

    adicionando interface

---
+++ src/app/home/home.component.ts
@@ -0,0 +1,18 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { HousingLocationComponent } from '../housing-location/housing-location.component';
+
+
+@Component({
+  selector: 'app-home',
+  standalone: true,
+  imports: [
+    HousingLocationComponent,
+    CommonModule,
+  ],
+  templateUrl: './home.component.html',
+  styleUrl: './home.component.css'
+})
+export class HomeComponent {
+
+}
