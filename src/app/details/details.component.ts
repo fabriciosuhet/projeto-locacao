581bc233266360527555135d0c354b90f2bd4100
Fabricio <fabriciossuhet@gmail.com>
2024-01-05 16:36:51 -0300

    adicionando pagina saiba mais

---
+++ src/app/details/details.component.ts
@@ -0,0 +1,26 @@
+import { Component, Inject, inject } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { ActivatedRoute } from '@angular/router';
+import { HousingLocation } from '../housing-location';
+import { HousingService } from '../housing.service';
+
+@Component({
+  selector: 'app-details',
+  standalone: true,
+  imports: [CommonModule,
+  ],
+  templateUrl: './details.component.html',
+  styleUrl: './details.component.css'
+})
+export class DetailsComponent {
+
+  route: ActivatedRoute = inject(ActivatedRoute);
+  housingService = inject(HousingService);
+  housingLocation: HousingLocation | undefined;
+  constructor() {
+    const housingLocationId = Number(this.route.snapshot.params['id']);
+    this.housingLocation =
+      this.housingService.getHousingLocationById(housingLocationId);
+  }
+
+}
