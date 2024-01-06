c36f75afcc03822f0cce92346a4a6663b485076e
Fabricio <fabriciossuhet@gmail.com>
2024-01-05 21:22:16 -0300

    adicionando formulario para locacao

--- src/app/details/details.component.ts
+++ src/app/details/details.component.ts
@@ -3,11 +3,14 @@ import { CommonModule } from '@angular/common';
 import { ActivatedRoute } from '@angular/router';
 import { HousingLocation } from '../housing-location';
 import { HousingService } from '../housing.service';
+import { FormControl, FormGroup, ReactiveFormsModule } from '@angular/forms';
+
 
 @Component({
   selector: 'app-details',
   standalone: true,
   imports: [CommonModule,
+    ReactiveFormsModule,
   ],
   templateUrl: './details.component.html',
   styleUrl: './details.component.css'
@@ -17,10 +20,25 @@ export class DetailsComponent {
   route: ActivatedRoute = inject(ActivatedRoute);
   housingService = inject(HousingService);
   housingLocation: HousingLocation | undefined;
+
+  applyForm = new FormGroup({
+    firstName: new FormControl(''),
+    lastName: new FormControl(''),
+    email: new FormControl('')
+  })
+
   constructor() {
     const housingLocationId = Number(this.route.snapshot.params['id']);
     this.housingLocation =
       this.housingService.getHousingLocationById(housingLocationId);
+  }
+
+  submitApplication() {
+    this.housingService.submitApplication(
+      this.applyForm.value.firstName ?? '',
+      this.applyForm.value.lastName ?? '',
+      this.applyForm.value.email ?? '',
+    );
   }
 
 }
