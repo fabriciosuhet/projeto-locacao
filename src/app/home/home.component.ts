2bfb72f20b11ee083eb02a7d1f798818328da7b8
Fabricio <fabriciossuhet@gmail.com>
2024-01-08 15:44:53 -0300

    realizando conexao http com banco de dados json

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -17,11 +17,26 @@ import { HousingLocation } from '../housing-location';
 })
 export class HomeComponent {
 
+  filteredLocationList: HousingLocation[] = [];
+
   housingLocationList: HousingLocation[] = [];
   housingService: HousingService = inject(HousingService);
 
   constructor() {
-    this.housingLocationList = this.housingService.getAllHousingLocations();
+    this.housingService.getAllHousingLocations().then((housingLocationList: HousingLocation[]) => {
+      this.housingLocationList = housingLocationList;
+      this.filteredLocationList = housingLocationList;
+    });
   }
 
+  filterResults(text: string) {
+    if (!text) {
+      this.filteredLocationList = this.housingLocationList;
+      return;
+    }
+
+    this.filteredLocationList = this.housingLocationList.filter(
+      housingLocation => housingLocation?.city.toLowerCase().includes(text.toLowerCase())
+    );
+  }
 }
